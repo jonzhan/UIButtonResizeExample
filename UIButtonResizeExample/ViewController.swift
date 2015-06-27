f2c493a4f5178242bb6842d331fe352f07b3f297
Jonathan Zhan <jonathan.zhan@me.com>
2015-06-27 16:59:05 +0800

    Initial Commit

---
+++ UIButtonResizeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIButtonResizeExample
+//
+//  Created by Jonathan on 27/6/15.
+//  Copyright (c) 2015 Chaotic Good. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
